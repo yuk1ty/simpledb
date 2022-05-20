7c37381234eb67de52a0e52a15fc93cd335fae38
yuk1ty <yuk1ty@users.noreply.github.com>
2022-05-20 17:14:44 +0900

    fix bugs

--- src/page.rs
+++ src/page.rs
@@ -1,4 +1,7 @@
-use bytes::{Buf, BufMut, Bytes, BytesMut};
+use std::{io::Cursor, mem::size_of};
+
+use byteorder::{BigEndian, ReadBytesExt};
+use bytes::{BufMut, Bytes, BytesMut};
 
 pub struct Page {
     bb: BytesMut,
@@ -17,26 +20,37 @@ impl Page {
         }
     }
 
-    pub fn get_int(&mut self, offset: usize) -> i32 {
-        self.bb.advance(offset);
-        self.bb.get_i32()
+    pub fn get_int(&self, offset: usize) -> i32 {
+        let freezed = self.bb.clone().freeze();
+        // TODO
+        let mut rdr = Cursor::new(freezed.slice(offset..(offset + size_of::<i32>())).to_vec());
+        rdr.read_i32::<BigEndian>().unwrap()
     }
 
     pub fn set_int(&mut self, offset: usize, n: i32) {
-        self.bb.advance(offset);
-        self.bb.put_i32(n);
+        let n = n.to_be_bytes();
+
+        unsafe {
+            // if offset is set, advance the cursor by the offset in advance
+            self.bb.advance_mut(offset);
+        }
+
+        for nn in n {
+            unsafe {
+                self.bb.chunk_mut().as_mut_ptr().write(nn);
+                self.bb.advance_mut(1);
+            }
+        }
     }
 
     pub fn get_bytes(&mut self, offset: usize) -> Option<&[u8]> {
-        self.bb.advance(offset);
-        let length = self.bb.get_i32() as usize;
-        // TODO ちょっと気になってる。あってる？
-        self.bb.get(..length)
+        let length = self.get_int(offset) as usize;
+        let bytes = self.bb.get(offset..(offset + length));
+        bytes
     }
 
     pub fn set_bytes(&mut self, offset: usize, b: &[u8]) {
-        self.bb.advance(offset);
-        self.bb.put_i32(b.len() as i32);
+        self.set_int(offset, b.len() as i32);
         self.bb.put(b);
     }
 
@@ -45,12 +59,30 @@ impl Page {
         b.map(|bytes| unsafe { String::from_utf8_unchecked(bytes.to_vec()) })
     }
 
+    pub fn set_string(&mut self, offset: usize, s: impl Into<String>) {
+        let text = s.into();
+        let b = text.as_bytes();
+        self.set_bytes(offset, b);
+    }
+
     pub(crate) fn contents(&mut self) -> Bytes {
-        self.bb.advance(0);
         let bb = std::mem::take(&mut self.bb);
         bb.freeze()
     }
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use crate::block_id::BlockId;
+
+    use super::Page;
+
+    #[test]
+    fn test_write() {
+        let block = BlockId::new("testfile", 2);
+        let mut p1 = Page::with_capacity(400);
+        let pos1: usize = 88;
+        p1.set_string(pos1, "abcdefghijklm");
+        println!("{}", p1.get_string(pos1).unwrap());
+    }
+}
